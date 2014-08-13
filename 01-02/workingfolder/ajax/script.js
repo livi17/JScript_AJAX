6ff1c455868b3ed8c7d655728fd88109cf1acab4
Michael LaRiviere <michael.lariviere@aceroinc.ca>
2014-08-12 22:20:44 -0400

    switched from project folder 01-02 to 01-03 and made changes to script.js

--- 01-02/workingfolder/ajax/script.js
+++ 01-02/workingfolder/ajax/script.js
@@ -1,6 +1,9 @@
-var request = new XMLHttpRequest();
-request.open('GET', 'data.txt', false);
-request.send();
-if (request.status===200) {
-	console.log(request);
+for (var i=0; i<100; i++){
+	var request = new XMLHttpRequest();
+	request.open('GET', 'data.txt', false);
+	request.send();7777
+	if (request.status===200) {
+		console.log(request);
+		document.writeln(request.responseText);
+	}
 }
